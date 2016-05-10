fe60915887f640b48ac636e3259653c69925458a
Brian Engelhardt <briatt22@gmail.com>
2016-05-10 00:07:42 -0500

    Add grunt task to build bookmarklets
    This will really help with building this in the future.

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-bookmarklet-wrapper');
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        bookmarklet_wrapper: {
+            options: {
+                // Task-specific options go here.
+                asJson: true
+            },
+            lite: {
+                files: {
+                    'bookmarklets/lite.bm.min.js': ['bookmarklets/lite.js']
+                }
+            },
+            list: {
+                files: {
+                    'bookmarklets/list.bm.min.js': ['bookmarklets/list.js']
+                }
+            }
+        },
+    });
+
+    grunt.registerTask('default', [
+        'bookmarklet_wrapper'
+    ]);
+};
